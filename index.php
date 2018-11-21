a6688b3c41ac6a69e8d679ea791528308f7e8dd8
MostafaMohamad <mostafa.mohamad.96.mm@gmail.com>
2018-11-21 14:31:12 +0200

    start

--- index.php
+++ index.php
@@ -3,22 +3,45 @@
 <head>
 	<meta charset="UTF-8">
 	
-	<!--JQuery js-->
-	<script src="js/jquery-3.3.1.js"></script>
+<!-- Style css -->
+<link href="css/style.css" type="text/css" rel="stylesheet">
 	
-	<!-- Bootstrap css -->
-	<link href="css/bootstrap.min.css" type="text/css" rel="stylesheet">
+	 <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="js/jquery-3.3.1.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="js/bootstrap.min.js"></script> 
 	
-	<!--Bootstrap js-->
-	<script src="js/bootstrap.min.js"></script>
+<!--Index js-->
+<script src="js/index.js"></script>
 	
-	<!--Index js-->
-	<script src="js/index.js"></script>
-	
-	<title>Technonew</title>
+<title>Technonew</title>
 	
 	
 </head>
 <body>
-</body>
+ <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1
+        <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Page 1-1</a></li>
+          <li><a href="#">Page 1-2</a></li>
+          <li><a href="#">Page 1-3</a></li>
+        </ul>
+      </li>
+      <li><a href="#">Page 2</a></li>
+      <li><a href="#">Page 3</a></li>
+    </ul>
+  </div>
+</nav> </body>
 </html>
