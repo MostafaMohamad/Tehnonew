6d2aee2066a5f6938a67ffb5654145f20417d3e4
MostafaMohamad <mostafa.mohamad.96.mm@gmail.com>
2018-12-01 13:59:16 +0200

    Stage1

--- index.php
+++ index.php
@@ -3,45 +3,15 @@
 <head>
 	<meta charset="UTF-8">
 	
-<!-- Style css -->
-<link href="css/style.css" type="text/css" rel="stylesheet">
+<?php
+		include("page.format.php");
+	?>
 	
-	 <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="css/bootstrap.min.css">
-
-<!-- jQuery library -->
-<script src="js/jquery-3.3.1.js"></script>
-
-<!-- Latest compiled JavaScript -->
-<script src="js/bootstrap.min.js"></script> 
-	
-<!--Index js-->
-<script src="js/index.js"></script>
-	
+	<script src="js/index.js"></script>
 <title>Technonew</title>
 	
 	
 </head>
 <body>
- <nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li class="dropdown">
-        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1
-        <span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a href="#">Page 1-1</a></li>
-          <li><a href="#">Page 1-2</a></li>
-          <li><a href="#">Page 1-3</a></li>
-        </ul>
-      </li>
-      <li><a href="#">Page 2</a></li>
-      <li><a href="#">Page 3</a></li>
-    </ul>
-  </div>
-</nav> </body>
+	</body>
 </html>
---
+++ page.format.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html>
+<head>
+	
+	
+	
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	
+	<!-- Style css -->
+	<link href="css/style.css" type="text/css" rel="stylesheet">
+
+	<!-- jQuery library -->
+	<script src="js/jquery-3.3.1.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="js/bootstrap.min.js"></script> 
+
+<meta charset="utf-8">
+</head>
+
+<body>
+	
+	 <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+    <div class="collapse navbar-collapse text-center" id="myNavbar">
+      <ul class="nav navbar-nav" id="ulnav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">Page 1</a></li>
+        <li><a href="#">Page 2</a></li>
+        <li><a href="#">Page 3</a></li>
+		<li><a href="#">Page 1</a></li>
+        <li><a href="#">Page 2</a></li>
+		  <li><a href="#">Page 2</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class=""></span> Sign Up</a></li>
+        <li><a href="#"><span></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav> 
+	
+</body>
+</html>
