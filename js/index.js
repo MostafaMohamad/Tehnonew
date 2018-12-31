6f9de61760cfc2f63c473355037b2da63f377a6e
MostafaMohamad <mostafa.mohamad.96.mm@gmail.com>
2018-12-31 10:15:07 +0200

    Design 0.1

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
+	'use strict';
+	$('.dropdown-submenu a.test').on("click", function(e){
+    $(this).next('ul').toggle();
+    e.stopPropagation();
+    e.preventDefault();
+  });
 
 });
